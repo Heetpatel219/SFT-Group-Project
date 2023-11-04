d36cd4c7fec88ef32bff98162cd443626fcc5a82
Priyangan Chandrathayan <pchandrathayan@myseneca.ca>
2023-11-03 22:10:53 -0400

    New structructures updated

---
+++ project-starter/map.h
@@ -0,0 +1,14 @@
+#ifndef MAP_H
+#define MAP_H
+
+#define MAP_ROWS 25
+#define MAP_COLS 25
+
+struct Map
+{
+    char squares[MAP_ROWS][MAP_COLS];
+    int numRows;
+    int numCols;
+};
+
+#endif
---
+++ project-starter/route.h
@@ -0,0 +1,15 @@
+#ifndef ROUTE_H
+#define ROUTE_H
+
+#include "point.h"
+
+#define MAX_ROUTE 100
+
+struct Route
+{
+    struct Point points[MAX_ROUTE];
+    int numPoints;
+    char routeSymbol;
+};
+
+#endif
